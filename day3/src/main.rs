9d89993c45af8fbebbfacada2b76fe919a258de2
Andrew Stanton-Nurse <andrew@andrewnurse.net>
2015-12-03 16:22:27 -0800

    add day3 part2

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -10,25 +10,31 @@ fn main() {
 }
 
 fn eval(content: &str) -> usize {
-    let mut x = 0;
-    let mut y = 0;
+    let (mut s_x, mut s_y) = (0, 0);
+    let (mut r_x, mut r_y) = (0, 0);
     let mut visited = HashSet::<(i64, i64)>::new();
 
     // Visit the first house
-    visited.insert((x, y));
-    println!("new visit: {},{}", x, y);
-    for c in content.chars() {
-        match c {
-            '^' => y -= 1,
-            '>' => x += 1,
-            '<' => x -= 1,
-            'v' => y += 1,
-            _ => continue // Skip invalid input
-        }
-        if visited.insert((x, y)) {
-            println!("new visit: {},{}", x, y);
+    visited.insert((s_x, s_y));
+    for (idx, c) in content.chars().enumerate() {
+        let (dx, dy) = match c {
+            '^' => (0, -1),
+            '>' => (1, 0),
+            '<' => (-1, 0),
+            'v' => (0, 1),
+            _ => continue
+        };
+
+        if idx % 2 == 0 {
+            // Santa
+            s_x += dx;
+            s_y += dy;
+            visited.insert((s_x, s_y));
         } else {
-            println!("re-visit : {},{}", x, y);
+            // Robo-santa
+            r_x += dx;
+            r_y += dy;
+            visited.insert((r_x, r_y));
         }
     }
 
