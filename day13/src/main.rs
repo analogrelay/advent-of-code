2a969e22cc002aa62904ac312e4a7d26448a3bd7
Andrew Stanton-Nurse <andrew@andrewnurse.net>
2015-12-15 20:11:34 -0800

    bruteforced day13

--- day13/src/main.rs
+++ day13/src/main.rs
@@ -3,6 +3,7 @@
 use std::collections::HashSet;
 
 extern crate adventutils;
+extern crate permutohedron;
 
 #[derive(Clone)]
 struct Entry {
@@ -11,82 +12,65 @@ struct Entry {
     pub happiness: isize
 }
 
-struct HappinessList(Vec<Entry>, HashSet<String>);
-
-impl HappinessList {
-    pub fn new(entries: Vec<Entry>) -> HappinessList {
-        let people = entries.iter().map(|e| &e.subject).collect();
-        HappinessList(entries, people)
-    }
-
-    pub fn entries(&self) -> &Vec<Entry> {
-        self.0
-    }
-
-    pub fn people(&self) -> &HashSet<String> {
-        self.1
-    }
-}
-
-struct Table(Vec<String>);
-
-impl Table {
-    pub fn new() -> Table {
-        Table(Vec::new())
-    }
-
-    pub fn seat(&mut self, entry: &str) {
-        self.0.push(entry.to_string());
-    }
-
-    pub fn neighbors(&mut self, person: &str) -> (Option<&str>, Option<&str>) {
-        let pos = match self.0.iter().position(|p| p == person) {
-            Some(p) => p,
-            None => return (None, None)
-        };
-
-        (self.get_neighbor(pos, -1), self.get_neighbor(pos, 1))
-    }
-
-    fn get_neighbor(&self, current: usize, offset: isize) -> Option<&str> {
-        let pos = current as isize + offset;
-        let idx = if pos < 0 {
-            self.0.len() as isize + pos
-        } else {
-            pos
-        } as usize;
-
-        if idx == current {
-            None
-        } else {
-            Some(&self.0[idx])
-        }
-    }
-}
-
 fn main() {
     let input = adventutils::read_input_file();
-    let happiness = HappinessList::new(input.lines().map(&parse_line).collect());
-    let mut people = happiness.people().clone();
-    let mut table = Table::new();
-    let mut current : Option<&str> = None;
+    let mut entries : Vec<_> = input.lines().map(&parse_line).collect();
 
-    while !people.is_empty() {
-        // Find the best starting entry and place it
-        let best_entry = entries.iter()
-            .filter(|e| people.contains(&e.other) && (current.is_none() || current.unwrap() == e.subject))
-            .max_by(|e| e.happiness)
-            .unwrap();
-        table.seat(&best_entry.other);
+    let result = calculate_best_happiness(entries.clone());
+    println!("Part 1 Max happiness is: {}", result);
 
-        // Set up for the next iteration
-        current = Some(&best_entry.other);
-        people.remove(&best_entry.other);
+    // Add myself to the list
+    let people : Vec<_> = get_distinct_people(&entries);
+    for person in people {
+        entries.push(Entry {
+            subject: person.to_string(),
+            other: "Andrew".to_string(),
+            happiness: 0
+        });
+        entries.push(Entry {
+            subject: "Andrew".to_string(),
+            other: person.to_string(),
+            happiness: 0
+        });
     }
+    let result = calculate_best_happiness(entries);
+    println!("Part 2 Max happiness is: {}", result);
+}
+
+fn get_distinct_people(entries: &Vec<Entry>) -> Vec<String> {
+    let people_hash : HashSet<_> = entries.iter().map(|e| e.subject.clone()).collect();
+    people_hash.iter().cloned().collect()
+}
+
+fn calculate_best_happiness(entries: Vec<Entry>) -> isize {
+    let mut people = get_distinct_people(&entries);
+
+    // Permute all the people
+    let permutations = permutohedron::Heap::new(&mut people);
+    permutations
+        .map(|p| calculate_happiness(&p, &entries))
+        .max()
+        .unwrap()
+}
+
+fn calculate_happiness(order: &Vec<String>, entries: &Vec<Entry>) -> isize {
+    let mut total = 0;
+    for (idx, person) in order.iter().enumerate() {
+        let neighbor = if idx < order.len() - 1 {
+            &order[idx + 1]
+        } else {
+            &order[0]
+        };
+
+        total += entries.iter().find(|e| e.subject == *person && e.other == *neighbor).unwrap().happiness;
+        total += entries.iter().find(|e| e.other == *person && e.subject == *neighbor).unwrap().happiness;
+    }
+    total
 }
 
 fn parse_line(line: &str) -> Entry {
     let tokens : Vec<_> = line.split_whitespace().collect();
+
     match tokens.as_slice() {
         [ subject, "would", "gain", happiness, "happiness", "units", "by", "sitting", "next", "to", other ] => Entry {
             subject: subject.into(),
