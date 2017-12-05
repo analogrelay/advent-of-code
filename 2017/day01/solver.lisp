c3766aa7ce3d644e0f7133abdaca1dd64d86a8f6
Andrew Stanton-Nurse <andrew@andrewnurse.net>
2017-12-04 20:13:24 -0800

    some docs

--- 2017/day01/solver.lisp
+++ 2017/day01/solver.lisp
@@ -1,8 +1,12 @@
+;;; Parse the input as a sequence of integers
+
 (defun parse-input (filename)
   (with-open-file (in filename)
     (loop for c = (read-char in nil)
 	 while (and (char>= c #\0) (char<= c #\9))
 	 collect (parse-integer (string c)))))
+
+;;; Solve the "captcha" provided in the specified list
 
 (defun solve-captcha (input)
   (getf (reduce (lambda (state next)
@@ -14,6 +18,8 @@
 	  input
 	  :initial-value (list :sum 0 :last (first (last input))))
 	:sum))
+
+;;; Run the day 1a solution on the specified input file
 
 (defun run-day1a (filename)
   (solve-captcha (parse-input filename)))
