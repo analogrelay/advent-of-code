dcf86b0cc0078230fc5e7ed9df32f1ea5f0cf905
Andrew Stanton-Nurse <andrew@andrewnurse.net>
2017-12-07 08:17:23 -0800

    finish day4b

--- 2017/day04/day4.lisp
+++ 2017/day04/day4.lisp
@@ -7,8 +7,23 @@
        collect
 	 (split-sequence:split-sequence #\Space l))))
 
-(defun check-passphrase (phrase)
-  (member-if (lambda (x) (> (count x phrase :test #'string=)) 1)) phrase))
+(defun check-passphrase (test phrase)
+  (every (lambda (x) (funcall test x phrase)) phrase))
+
+(defun has-duplicate (item phrase)
+  (> (count item phrase :test #'string=) 1))
+
+(defun has-anagram (item phrase)
+  (> (count item phrase :test #'anagramp) 1))
+
+(defun anagramp (l r)
+  (equal (sort (copy-seq l) #'string<) (sort (copy-seq r) #'string<)))
+
+(defun count-passphrases (test list)
+  (count-if (lambda(x) (check-passphrase test x)) list))
 
 (defun run-day4a (filename)
-  (count-if #'check-passphrase (parse-input filename)))
+  (count-passphrases #'has-duplicate (parse-input filename)))
+
+(defun run-day4b (filename)
+  (count-passphrases #'has-anagram (parse-input filename)))
