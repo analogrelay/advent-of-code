854f2ee658319e4b52973be27a93f359e6a7ab2d
Andrew Stanton-Nurse <andrew@andrewnurse.net>
2017-12-11 17:14:23 -0800

    day11 done, super easy

--- 2017/day11/day11.lisp
+++ 2017/day11/day11.lisp
@@ -22,5 +22,33 @@
     (:se (list 1 0 -1))
     (:s (list 1 -1 0))
     (:sw (list 0 -1 1))
-    (:se (list -1 0 1))))
-    
+    (:nw (list -1 0 1))
+    (t (error "Unknown direction: ~a" dir))))
+
+(defun get-distance (l r)
+  (destructuring-bind (lx ly lz) l
+    (destructuring-bind (rx ry rz) r
+      (/ (+ (abs (- rx lx))
+	    (abs (- ry ly))
+	    (abs (- rz lz)))
+	 2))))
+
+(defun hexstep (position direction)
+  (destructuring-bind (x y z) position
+    (destructuring-bind (dx dy dz) (get-offset direction)
+      (list (+ x dx) (+ y dy) (+ z dz)))))
+
+(defun hexwalk (start directions)
+  (let ((pos start)
+	(max-distance 0))
+    (dolist (dir directions)
+      (setf pos (hexstep pos dir))
+      (let ((new-distance (get-distance start pos)))
+	(when (> new-distance max-distance)
+	  (setf max-distance new-distance))))
+    (values pos max-distance)))
+
+(defun run-day11 (filename)
+  (multiple-value-bind (end-pos max-distance) (hexwalk (list 0 0 0) (parse-input filename))
+    (values (get-distance (list 0 0 0) end-pos)
+	    max-distance)))
