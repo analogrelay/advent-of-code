aa303014e12e47bfc4b52c480ee7d02429cd2a64
Andrew Stanton-Nurse <andrew@andrewnurse.net>
2017-12-11 17:11:59 -0800

    day11a done

--- 2017/day11/day11.lisp
+++ 2017/day11/day11.lisp
@@ -13,3 +13,14 @@
 (defun parse-input (filename)
   (with-open-file (in filename)
     (mapcar #'parse-direction (split-sequence:split-sequence #\, (read-line in nil)))))
+
+;;; Get the cube-coordinate offset given a direction
+(defun get-offset (dir)
+  (case dir
+    (:n (list -1 1 0))
+    (:ne (list 0 1 -1))
+    (:se (list 1 0 -1))
+    (:s (list 1 -1 0))
+    (:sw (list 0 -1 1))
+    (:se (list -1 0 1))))
+    
