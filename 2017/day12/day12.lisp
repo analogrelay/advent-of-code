df9defc574212658ce9fd16ab3d530c696e1ca5f
Andrew Stanton-Nurse <andrew@andrewnurse.net>
2017-12-13 21:32:54 -0800

    day12b
    use union-find algorithm to build spanning trees to solve both parts of day12

--- 2017/day12/day12.lisp
+++ 2017/day12/day12.lisp
@@ -11,48 +11,77 @@
 	(split-seq (search candidate seq) seq (length candidate))
 	(values seq nil))))
 
-;;; Adds a unidirectional link from l to r in the table
-(defun add-link (l r table)
-  (setf (gethash l table)
-	(adjoin r (gethash l table))))
-
 (defun dump-table (table)
   (maphash (lambda (k v) (format t "~a -> ~a~%" k v))
 	   table))
 
 ;;; Gets a list of all nodes reachable from the specified node
-(defun expand-reachable (node table)
-  (do ((work (list node))
-       (reachable nil))
-      ((not work) reachable)
-    (let* ((next (pop work))
-	   (children (gethash next table)))
-      (dolist (child children)
-	(when (not (member child reachable))
-	  ;; Found a node not already reachable, add to work list
-	  (push child work)
-	  ;; And mark reachable
-	  (push child reachable))))))
-
 (defun parse-input (filename)
   (with-open-file (in filename)
     (do ((line (read-line in nil) (read-line in nil))
-	 (links (make-hash-table)))
-	((not line) links)
-      (multiple-value-bind (left right) (split-at " <-> " line)
-	(let ((leftidx (parse-integer left)))
-	  (dolist (target (split-sequence:split-sequence #\, right))
-	    (let ((targetidx (parse-integer target)))
-	      (add-link leftidx targetidx links)
-	      (add-link targetidx leftidx links))))))))
+	 (values (make-array 2000 :fill-pointer 0)))
+	((not line) values)
+      (multiple-value-bind (left-str right-list) (split-at " <-> " line)
+	(vector-push-extend
+	 (list :left (parse-integer left-str)
+	       :right (mapcar #'parse-integer (split-sequence:split-sequence #\, right-list)))
+	 values)))))
 
-(defun expand-node-table (table)
-  (let ((new-table (make-hash-table)))
-    (maphash (lambda (k v)
-	       (declare (ignore v))
-	       (setf (gethash k new-table) (expand-reachable k table)))
-	     table)
-    new-table))
 
-(defun run-day12a (filename)
-  (length (gethash 0 (expand-node-table (parse-input filename)))))
+(defclass cell () ((program-id :accessor program-id
+			       :initarg :program-id
+			       :initform (error "program-id must be specified")
+			       :type 'integer)
+		   (parent :accessor parent
+			   :initform nil
+			   :type 'cell)
+		   (node-rank :accessor node-rank
+			      :initform 0
+			      :type 'integer)
+		   (children :accessor children
+			     :initform nil
+			     :type 'list)))
+
+(defmethod print-object ((cell cell) stream)
+  (format stream "[~a (-^ ~a; #~a)]"
+	  (program-id cell)
+	  (if (parent cell)
+	      (program-id (parent cell))
+	      nil)
+	  (node-rank cell)))
+
+(defun make-cell (id) (make-instance 'cell :program-id id))
+
+(defun get-cell (idx items)
+  (when (not (elt items idx))
+    (setf (elt items idx) (make-cell idx)))
+  (elt items idx))
+
+(defun find-root (item)
+  (if (parent item)
+      (find-root (parent item))
+      item))
+
+(defun set-parent (c p)
+  (setf (parent c) p)
+  (setf (children p)
+	(append (list c) (children p))))
+
+(defun add-edge (left right)
+  (let ((lroot (find-root left))
+	(rroot (find-root right)))
+    (unless (= (program-id lroot) (program-id rroot))
+      (cond ((< (node-rank lroot) (node-rank rroot)) (set-parent lroot rroot))
+	    ((> (node-rank lroot) (node-rank rroot)) (set-parent rroot lroot))
+	    (t (progn
+		 (incf (node-rank lroot))
+		 (set-parent rroot lroot)))))))
+
+(defun generate-spanning-trees (edges)
+  (let ((items (make-array (length edges) :initial-element nil)))
+    (dotimes (i (length edges))
+      (let ((edge (elt edges i)))
+	(let ((left (get-cell (getf edge :left) items)))
+	(dolist (right (getf edge :right))
+	  (add-edge left (get-cell right items))))))
+    items))
