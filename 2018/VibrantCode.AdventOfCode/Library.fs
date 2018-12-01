abdbfa04e369af59b7e7065ffcd29e3915909733
Andrew Stanton-Nurse <andrew@stanton-nurse.com>
2018-12-01 10:35:11 -0800

    add initial structure

---
+++ 2018/VibrantCode.AdventOfCode/Library.fs
@@ -0,0 +1,5 @@
+namespace VibrantCode.AdventOfCode
+
+module Say =
+    let hello name =
+        sprintf "Hello %s" name
---
+++ 2018/day01/Program.fs
@@ -0,0 +1,9 @@
+// Learn more about F# at http://fsharp.org
+
+open System
+open VibrantCode.AdventOfCode
+
+[<EntryPoint>]
+let main argv =
+    Say.hello "world" |> Console.WriteLine
+    0
