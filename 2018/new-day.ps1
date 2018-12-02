30165369bf29e6151d849dd1b0cb8931d4ed9288
Andrew Stanton-Nurse <andrew@stanton-nurse.com>
2018-12-01 22:20:37 -0800

    add day03 template and new day script

---
+++ 2018/new-day.ps1
@@ -0,0 +1,50 @@
+param([Parameter(Mandatory = $true, Position = 0)][int]$DayNumber)
+
+Push-Location $PSScriptRoot
+try {
+    $name = "day$($DayNumber.ToString("00"))"
+    dotnet new console --language "F#" --name $name
+    dotnet sln add $name
+    dotnet add $name reference "./VibrantCode.AdventOfCode"
+
+    "" | Add-Content -Path "./$name/input.txt"
+    "" | Add-Content -Path "./$name/test.txt"
+
+    $LaunchSettingsContent = @"
+{
+  "profiles": {
+    "$name": {
+      "commandName": "Project",
+      "commandLineArgs": "`$(MSBuildProjectDirectory)/input.txt"
+    },
+    "$name - test": {
+      "commandName": "Project",
+      "commandLineArgs": "`$(MSBuildProjectDirectory)/test.txt"
+    }
+  }
+}
+"@
+    mkdir "./$name/Properties" | Out-Null
+
+    $ProgramFsContent = @"
+open System
+open VibrantCode.AdventOfCode
+
+let run data = 
+    raise (new NotImplementedException())
+
+[<EntryPoint>]
+let main argv =
+    argv.[0]
+    |> AdventHelpers.loadLines
+    |> Seq.map int
+    |> run
+
+    0
+"@
+    $LaunchSettingsContent | Set-Content -Path "./$name/Properties/launchSettings.json" -Encoding UTF8
+    $ProgramFsContent | Set-Content -Path "./$name/Program.fs" -Encoding UTF8
+}
+finally {
+    Pop-Location
+}
