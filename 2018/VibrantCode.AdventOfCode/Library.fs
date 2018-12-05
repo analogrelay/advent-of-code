ad53f46cd7901161aaa04088b425f876748cb6d5
Andrew Stanton-Nurse <andrew@stanton-nurse.com>
2018-12-05 10:56:32 -0800

    day05 part 1

--- 2018/VibrantCode.AdventOfCode/Library.fs
+++ 2018/VibrantCode.AdventOfCode/Library.fs
@@ -3,6 +3,8 @@ namespace VibrantCode.AdventOfCode
 module AdventHelpers =
     open System.IO
 
+    let loadString (file: string) = File.ReadAllText(file)
+
     let loadLines (file: string) = seq {
         use stream = new StreamReader(file)
         while not stream.EndOfStream do
--- 2018/day05/Program.fs
+++ 2018/day05/Program.fs
@@ -1,14 +1,35 @@
 open System
 open VibrantCode.AdventOfCode
+open System
 
-let run data = 
-    raise (new NotImplementedException())
+let areReactive (l: char) (r: char) = l <> r && ((Char.ToLower l) = (Char.ToLower r))
+
+let rec processList (count: int) (lookbehind: char list) (data: char list) = 
+    // We maintain two lists, the main list and the lookbehind list
+    // The lookbehind list is in _REVERSE_ order (so the head of the list is the
+    // immediate previous item)
+
+    match (lookbehind, data) with
+    // There's a match!
+    // Deduct one from the count and continue
+    | (l :: lbtail, r :: tail) when areReactive l r -> processList (count - 1) lbtail tail
+
+    // No matches, move forward
+    | (lb, head :: tail) -> processList (count + 1) (head :: lb) tail
+
+    // Empty list, end case.
+    | (_, []) -> count 
+
+let run (data: char list) =
+    data
+    |> processList 0 []
+    |> printfn "Part 1: %d"
 
 [<EntryPoint>]
 let main argv =
     argv.[0]
-    |> AdventHelpers.loadLines
-    |> Seq.map int
+    |> AdventHelpers.loadString
+    |> List.ofSeq
     |> run
 
     0
