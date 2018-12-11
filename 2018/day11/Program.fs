c85414a79805ed80e2f343544b1efadeb6addd5e
Andrew Stanton-Nurse <andrew@stanton-nurse.com>
2018-12-11 14:14:25 -0800

    better day11

--- 2018/day11/Program.fs
+++ 2018/day11/Program.fs
@@ -1,35 +1,19 @@
-open System
-open VibrantCode.AdventOfCode.AdventHelpers
+open System.Diagnostics
 
-// With the test data, I observed that there's a "peak" to the powerlevels by size. So,
-// the best powerlevel is the first "maxima" that is found in order. So as the size increases,
-// so does the powerlevel, but as soon as it drops, we have the final answer (the value immediately
-// before the drop).
+// Two tricks are used here to reduce run time for part 2:
+// 1. The data show a "peak" power level, so that once the power level starts decreasing, it will always decrease
+//    This means once we find the first size that has a *lower* power level than it's predecessor, it's predecessor is the best result
+// 2. We use a Summed-Area Table (https://en.wikipedia.org/wiki/Summed-area_table), computed once, to rapidly compute sums for particular rectangles
 
 /// <summary>
 /// Enumerates the top-left corner coordinates of all the 3x3 squares in the grid
 /// </summary>
 let enumerateSquares (size: int) =
-    let edge = 300 - (size - 1);
     seq {
-        for x in 1..edge do
-            for y in 1..edge do
+        for x in size..300 do
+            for y in size..300 do
                 yield (x, y)
     }
-
-/// <summary>
-/// Generates all the cells contained within the square identified by the cell at the specified coordinate
-/// </summary>
-/// <param name="x">The x coordinate of the top-level cell in the square.</param>
-/// <param name="y">The y coordinate of the top-level cell in the square.</param>
-let regionForCell size (x0, y0) =
-    let xend = (x0 + size) - 1
-    let yend = (y0 + size) - 1
-    seq {
-        for x in x0..xend do
-            for y in y0..yend do
-                yield (x, y)
-    } |> List.ofSeq
 
 /// <summary>
 /// Extract the hundreds digit for a number.
@@ -52,13 +36,45 @@ let powerLevelForCell serialNumber (x, y) =
     step5 - 5
 
 /// <summary>
-/// Determines the power level for a single 3x3 square in the grid.
+/// Determines the power level for a single square in the grid.
 /// </summary>
 /// <param name="serialNumber">The serial number for the grid.</param>
 /// <param name="region">The set of squares contained in the grid.</param>
-let powerLevelForSquare serialNumber size (x, y) =
-    regionForCell size (x, y) |> Seq.sumBy (powerLevelForCell serialNumber)
+let powerLevelForSquare (sumTable: int[,]) size (x, y) =
+    // Computes the sum using Summed-area table algorithm
+    // See: https://en.wikipedia.org/wiki/Summed-area_table
+    let sa = sumTable.[x - size, y - size]
+    let sb = sumTable.[x, y - size]
+    let sc = sumTable.[x - size, y]
+    let sd = sumTable.[x, y]
+    let res = sd + sa - sb - sc
+    res
 
+/// <summary>
+/// Generates a summed-area table (https://en.wikipedia.org/wiki/Summed-area_table) for the specified serial number
+/// </summary>
+/// <param name="serialNumber">The serial number of the grid to generate</param>
+let generateSumTable serialNumber =
+    let mutable arr: int[,] = Array2D.zeroCreate 301 301
+    for y in 1..300 do
+        for x in 1..300 do
+            let myVal = powerLevelForCell serialNumber (x, y)
+            let newVal = myVal + arr.[x, y - 1] + arr.[x - 1, y] - arr.[x - 1, y - 1]
+            arr.[x,y] <- newVal
+    arr
+
+let generatePowerGrid serialNumber =
+    Array2D.init 300 300 (fun x y -> powerLevelForCell serialNumber (x + 1, y + 1))
+
+let findBestSquare (sumTable: int[,]) (size: int) =
+    let ((x1, y1), powerLevel) =
+        enumerateSquares size
+        |> Seq.map (fun coord -> (coord, powerLevelForSquare sumTable size coord))
+        |> Seq.maxBy snd
+
+    ((x1 - size + 1, y1 - size + 1), powerLevel)
+
+    
 let firstMaximaBy func seq =
     let mutable current = None
     let mutable currentVal = None
@@ -77,23 +93,20 @@ let firstMaximaBy func seq =
 
 let run (serialNumber: int) = 
     printfn "Serial Number: %d" serialNumber
-    let ((x, y), powerLevel) =
-        enumerateSquares 3
-        |> Seq.map (fun regionId -> (regionId, powerLevelForSquare serialNumber 3 regionId))
-        |> Seq.maxBy snd
-    printfn "Part 1: %d (at %d, %d)" powerLevel x y
+    let sumTable = generateSumTable serialNumber
 
-    let ((x, y, size), powerLevel) =
+    let ((x1, y1), powerLevel) = findBestSquare sumTable 3
+    printfn "Part 1: %d,%d (level: %d)" x1 y1 powerLevel
+
+    let time = Stopwatch.StartNew()
+    let ((x1, y1, size), powerLevel) =
         seq { 1..300 }
         |> Seq.map (fun size ->
-            let ((x, y), value) =
-                enumerateSquares size
-                |> Seq.map (fun regionId -> (regionId, powerLevelForSquare serialNumber size regionId))
-                |> Seq.maxBy snd
-            printfn "Max for size %2d: %d (at %d, %d)" size value x y
-            ((x, y, size), value))
+            let ((x1, y1), powerLevel) = findBestSquare sumTable size
+            ((x1, y1, size), powerLevel))
         |> firstMaximaBy snd
-    printfn "Part 2: %d (at %d, %d, %d)" powerLevel x y size
+    let elapsed = time.ElapsedMilliseconds
+    printfn "Part 2: %d,%d,%d (level: %d) (elapsed: %dms)" x1 y1 size powerLevel elapsed
 
 [<EntryPoint>]
 let main argv =
