6488f31d7fb0832fe7e228381a24ed20e37a5fc4
Andrew Stanton-Nurse <andrew@stanton-nurse.com>
2020-12-05 10:29:40 -0800

    simpler day5

--- 2020/day05.go
+++ 2020/day05.go
@@ -23,7 +23,7 @@ func day05(args []string) error {
 	maxSeatID := 0
 	presentSeats := make([]int, 0, len(lines))
 	for _, line := range lines {
-		seatID, err := getSeatID(line)
+		seatID := getSeatID(line)
 		if err != nil {
 			return fmt.Errorf("error computing seat ID '%s': %v", line, err)
 		}
@@ -54,73 +54,14 @@ func day05(args []string) error {
 	return nil
 }
 
-type space struct {
-	start int
-	end   int
-}
-
-func newSpace(start, end int) space {
-	return space{start, end}
-}
-
-func (s *space) partition(low bool) space {
-	newLen := (s.end - s.start) / 2
-	if low {
-		return space{
-			start: s.start,
-			end:   s.start + newLen,
-		}
-	} else {
-		return space{
-			start: s.end - newLen,
-			end:   s.end,
+// Shoulda noticed this first, but meh.
+func getSeatID(boardingPass string) int {
+	var seatID int
+	for _, c := range boardingPass {
+		seatID <<= 1
+		if c == 'B' || c == 'R' {
+			seatID++
 		}
 	}
-}
-
-func (s *space) getValue(sequence []bool) (int, error) {
-	current := *s
-	for _, low := range sequence {
-		current = current.partition(low)
-	}
-
-	if current.len() != 1 {
-		return 0, fmt.Errorf("sequence (%v-%v) did not result in a single item", current.start, current.end)
-	}
-	return current.start, nil
-}
-
-func (s *space) len() int {
-	return s.end - s.start + 1
-}
-
-func parseSequence(sequence string) []bool {
-	// We just assume you're passing a single-type sequence in here, either F/B or L/R.
-	output := make([]bool, 0, len(sequence))
-	for _, c := range sequence {
-		output = append(output, c == 'F' || c == 'L')
-	}
-	return output
-}
-
-var rowSpace = newSpace(0, 127)
-var colSpace = newSpace(0, 7)
-
-func getSeatID(boardingPass string) (int, error) {
-	if len(boardingPass) != 10 {
-		return 0, fmt.Errorf("boarding pass should be 10 characters")
-	}
-	row := parseSequence(boardingPass[0:7])
-	col := parseSequence(boardingPass[7:])
-
-	rowNumber, err := rowSpace.getValue(row)
-	if err != nil {
-		return 0, fmt.Errorf("row sequence invalid: %v", err)
-	}
-	colNumber, err := colSpace.getValue(col)
-	if err != nil {
-		return 0, fmt.Errorf("col sequence invalid: %v", err)
-	}
-
-	return rowNumber*8 + colNumber, nil
+	return seatID
 }
--- 2020/day05_test.go
+++ 2020/day05_test.go
@@ -7,101 +7,6 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestPartition(t *testing.T) {
-	tests := map[string]struct {
-		initial  space
-		low      bool
-		expected space
-	}{
-		"lower half": {
-			initial:  newSpace(0, 127),
-			low:      true,
-			expected: newSpace(0, 63),
-		},
-		"upper half": {
-			initial:  newSpace(0, 63),
-			low:      false,
-			expected: newSpace(32, 63),
-		},
-		"two numbers - low": {
-			initial:  newSpace(44, 45),
-			low:      true,
-			expected: newSpace(44, 44),
-		},
-		"two numbers - high": {
-			initial:  newSpace(44, 45),
-			low:      false,
-			expected: newSpace(45, 45),
-		},
-	}
-
-	for name, tc := range tests {
-		t.Run(name, func(t *testing.T) {
-			new := tc.initial.partition(tc.low)
-			require.Equal(t, tc.expected, new)
-		})
-	}
-}
-
-func TestGetValue(t *testing.T) {
-	tests := map[string]struct {
-		initial  space
-		sequence []bool
-		expected int
-		err      error
-	}{
-		"not enough in sequence": {
-			initial:  newSpace(0, 127),
-			sequence: []bool{true, false},
-			expected: 0,
-			err:      fmt.Errorf("sequence (32-63) did not result in a single item"),
-		},
-		"example1rows": {
-			initial:  newSpace(0, 127),
-			sequence: parseSequence("FBFBBFF"),
-			expected: 44,
-		},
-		"example2rows": {
-			initial:  newSpace(0, 127),
-			sequence: parseSequence("BFFFBBF"),
-			expected: 70,
-		},
-		"example3rows": {
-			initial:  newSpace(0, 127),
-			sequence: parseSequence("FFFBBBF"),
-			expected: 14,
-		},
-		"example4rows": {
-			initial:  newSpace(0, 127),
-			sequence: parseSequence("BBFFBBF"),
-			expected: 102,
-		},
-		"example1seats": {
-			initial:  newSpace(0, 7),
-			sequence: parseSequence("RLR"),
-			expected: 5,
-		},
-		"example2and3seats": {
-			initial:  newSpace(0, 7),
-			sequence: parseSequence("RRR"),
-			expected: 7,
-		},
-		"example4seats": {
-			initial:  newSpace(0, 7),
-			sequence: parseSequence("RLL"),
-			expected: 4,
-		},
-	}
-
-	for name, tc := range tests {
-		t.Run(name, func(t *testing.T) {
-			result, err := tc.initial.getValue(tc.sequence)
-			require.Equal(t, tc.err, err)
-			require.Equal(t, tc.expected, result)
-		})
-	}
-}
-
 func TestBoardingPasses(t *testing.T) {
 	tests := map[string]int{
 		"FBFBBFFRLR": 357,
@@ -112,8 +17,7 @@ func TestBoardingPasses(t *testing.T) {
 
 	for sequence, expected := range tests {
 		t.Run(fmt.Sprint("test", sequence), func(t *testing.T) {
-			seatID, err := getSeatID(sequence)
-			require.Nil(t, err)
+			seatID := getSeatID(sequence)
 			require.Equal(t, expected, seatID)
 		})
 	}
