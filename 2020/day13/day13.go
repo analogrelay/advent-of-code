b07e0b5f7e6e4c21c7994081048c4e25fc88d93c
Andrew Stanton-Nurse <andrew@stanton-nurse.com>
2020-12-17 16:02:48 -0800

    day13 done!

--- 2020/day13/day13.go
+++ 2020/day13/day13.go
@@ -1,7 +1,102 @@
 package day13
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+	"strconv"
+	"strings"
+)
 
 func Run(lines []string) error {
-    return fmt.Errorf("not yet implemented")
+	timestamp, err := strconv.Atoi(lines[0])
+	if err != nil {
+		return fmt.Errorf("invalid timestamp '%s'", lines[0])
+	}
+	busIDs, offsets, err := parseBusIDs(lines[1])
+	if err != nil {
+		return fmt.Errorf("error parsing bus IDs: %v", err)
+	}
+
+	id, departure, err := ComputeBestDeparture(timestamp, busIDs)
+	if err != nil {
+		return err
+	}
+	waitTime := departure - timestamp
+	result := id * waitTime
+
+	fmt.Println("Part 1:", result)
+
+	sequentialDeparture := ComputeSequentialDeparture(busIDs, offsets)
+	fmt.Println("Part 2:", sequentialDeparture)
+
+	return nil
+}
+
+func parseBusIDs(input string) ([]int, []int, error) {
+	splat := strings.Split(input, ",")
+	ids := []int{}
+	offsets := []int{}
+	for idx, idstr := range splat {
+		if idstr != "x" {
+			id, err := strconv.Atoi(idstr)
+			if err != nil {
+				return nil, nil, fmt.Errorf("invalid bus ID '%s'", idstr)
+			}
+			ids = append(ids, id)
+			offsets = append(offsets, idx)
+		}
+	}
+	return ids, offsets, nil
+}
+
+func ComputeSequentialDeparture(ids []int, offsets []int) int64 {
+	incrementor := int64(ids[0])
+	timeIndex := int64(incrementor)
+	foundMatch := make([]bool, len(offsets))
+	foundMatch[0] = true
+	remainingIntervals := len(ids) - 1
+	for {
+		for idx := range ids {
+			if (timeIndex+int64(offsets[idx]))%int64(ids[idx]) == 0 {
+				if !foundMatch[idx] {
+					incrementor *= int64(ids[idx])
+					foundMatch[idx] = true
+					remainingIntervals -= 1
+				}
+			}
+		}
+
+		if remainingIntervals == 0 {
+			if isValidDeparture(timeIndex, ids, offsets) {
+				return timeIndex
+			} else {
+				panic("something went horribly wrong")
+			}
+		}
+
+		timeIndex += incrementor
+	}
+}
+
+func isValidDeparture(timeIndex int64, ids []int, offsets []int) bool {
+	for idx := range ids {
+		if (timeIndex+int64(offsets[idx]))%int64(ids[idx]) != 0 {
+			return false
+		}
+	}
+	return true
+}
+
+func ComputeBestDeparture(timestamp int, busIDs []int) (int, int, error) {
+	bestID := -1
+	bestDeparture := -1
+	for _, id := range busIDs {
+		bestDivisor := int(math.Ceil(float64(timestamp) / float64(id)))
+		departure := bestDivisor * id
+		if bestDeparture == -1 || departure < bestDeparture {
+			bestID = id
+			bestDeparture = departure
+		}
+	}
+	return bestID, bestDeparture, nil
 }
--- 2020/day13/day13_test.go
+++ 2020/day13/day13_test.go
@@ -1,1 +1,42 @@
 package day13
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestPart1(t *testing.T) {
+	busIds, remainders, err := parseBusIDs("7,13,x,x,59,x,31,19")
+	require.NoError(t, err)
+	require.Equal(t, []int{7, 13, 59, 31, 19}, busIds)
+	require.Equal(t, []int{0, 1, 4, 6, 7}, remainders)
+	id, departure, err := ComputeBestDeparture(939, busIds)
+	require.NoError(t, err)
+	require.Equal(t, 59, id)
+	require.Equal(t, 944, departure)
+}
+
+var testCases = []struct {
+	sequentialDeparture int64
+	ids                 []int
+	offsets             []int
+}{
+	{3417, []int{17, 13, 19}, []int{0, 2, 3}},
+	{754018, []int{67, 7, 59, 61}, []int{0, 1, 2, 3}},
+	{779210, []int{67, 7, 59, 61}, []int{0, 2, 3, 4}},
+	{1261476, []int{67, 7, 59, 61}, []int{0, 1, 3, 4}},
+	{1202161486, []int{1789, 37, 47, 1889}, []int{0, 1, 2, 3}},
+}
+
+func TestIsValidDeparture(t *testing.T) {
+	for _, testCase := range testCases {
+		require.True(t, isValidDeparture(testCase.sequentialDeparture, testCase.ids, testCase.offsets))
+	}
+}
+
+func TestComputeSequentialDeparture(t *testing.T) {
+	for _, testCase := range testCases {
+		require.Equal(t, testCase.sequentialDeparture, ComputeSequentialDeparture(testCase.ids, testCase.offsets))
+	}
+}
