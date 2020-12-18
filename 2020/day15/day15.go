fbd0c0d1fe6091ed243f5538c6acecc3939f1851
Andrew Stanton-Nurse <andrew@stanton-nurse.com>
2020-12-18 05:35:17 +0000

    day 15 2020

--- 2020/day15/day15.go
+++ 2020/day15/day15.go
@@ -1,7 +1,76 @@
 package day15
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
 
 func Run(lines []string) error {
-    return fmt.Errorf("not yet implemented")
+	splat := strings.Split(lines[0], ",")
+	numbers := make([]int, 0, len(splat))
+	for _, s := range splat {
+		num, err := strconv.Atoi(s)
+		if err != nil {
+			return fmt.Errorf("invalid number '%s'", s)
+		}
+		numbers = append(numbers, num)
+	}
+
+	game := NewGame()
+	game.SpeakAll(numbers...)
+	lastNumber := game.PlayTo(2020)
+
+	fmt.Println("Part 1:", lastNumber)
+
+	lastNumber = game.PlayTo(30000000)
+
+	fmt.Println("Part 2:", lastNumber)
+	return nil
+}
+
+type GameState struct {
+	LastSpoken    int
+	TurnIndex     int
+	numbersSpoken map[int][]int
+}
+
+func NewGame() GameState {
+	return GameState{LastSpoken: 0, TurnIndex: 0, numbersSpoken: make(map[int][]int)}
+}
+
+func (g *GameState) PlayTo(index int) int {
+	for {
+		number := g.TakeTurn()
+		if g.TurnIndex == index {
+			return number
+		}
+	}
+}
+
+// TakeTurn takes a game turn and returns the number spoken in it
+func (g *GameState) TakeTurn() int {
+	if spokenAt, ok := g.numbersSpoken[g.LastSpoken]; ok && len(spokenAt) > 1 {
+		age := spokenAt[len(spokenAt)-1] - spokenAt[len(spokenAt)-2]
+		g.Speak(age)
+	} else {
+		g.Speak(0)
+	}
+	return g.LastSpoken
+}
+
+func (g *GameState) Speak(number int) {
+	g.TurnIndex++
+	g.LastSpoken = number
+	if list, ok := g.numbersSpoken[number]; ok {
+		g.numbersSpoken[number] = append(list, g.TurnIndex)
+	} else {
+		g.numbersSpoken[number] = []int{g.TurnIndex}
+	}
+}
+
+func (g *GameState) SpeakAll(numbers ...int) {
+	for _, num := range numbers {
+		g.Speak(num)
+	}
 }
--- 2020/day15/day15_test.go
+++ 2020/day15/day15_test.go
@@ -1,1 +1,54 @@
 package day15
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestGame(t *testing.T) {
+	game := NewGame()
+	game.SpeakAll(0, 3, 6)
+	require.Equal(t, 3, game.TurnIndex)
+
+	require.Equal(t, 0, game.TakeTurn())
+	require.Equal(t, 4, game.TurnIndex)
+
+	require.Equal(t, 3, game.TakeTurn())
+	require.Equal(t, 5, game.TurnIndex)
+
+	require.Equal(t, 3, game.TakeTurn())
+	require.Equal(t, 6, game.TurnIndex)
+
+	require.Equal(t, 1, game.TakeTurn())
+	require.Equal(t, 7, game.TurnIndex)
+
+	require.Equal(t, 0, game.TakeTurn())
+	require.Equal(t, 8, game.TurnIndex)
+
+	require.Equal(t, 4, game.TakeTurn())
+	require.Equal(t, 9, game.TurnIndex)
+
+	require.Equal(t, 0, game.TakeTurn())
+	require.Equal(t, 10, game.TurnIndex)
+}
+
+func TestExampleGames(t *testing.T) {
+	require.Equal(t, 1, playGame([]int{1, 3, 2}, 2020))
+	require.Equal(t, 10, playGame([]int{2, 1, 3}, 2020))
+	require.Equal(t, 27, playGame([]int{1, 2, 3}, 2020))
+	require.Equal(t, 78, playGame([]int{2, 3, 1}, 2020))
+	require.Equal(t, 438, playGame([]int{3, 2, 1}, 2020))
+	require.Equal(t, 1836, playGame([]int{3, 1, 2}, 2020))
+}
+
+func TestExampleJumboGames(t *testing.T) {
+	require.Equal(t, 175594, playGame([]int{0, 3, 6}, 30000000))
+}
+
+func playGame(startingNumbers []int, to int) int {
+	game := NewGame()
+	game.SpeakAll(startingNumbers...)
+	game.PlayTo(to)
+	return game.LastSpoken
+}
